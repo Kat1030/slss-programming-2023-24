ab4efa2a667da6f167bd9a1d5352fd32de2349e7
Katrina Chan <khcbunnies@gmail.com>
2023-11-07 13:45:54 -0800

    completed november 3 questions

--- Notes/semester_evaluator.py
+++ Notes/semester_evaluator.py
@@ -3,29 +3,22 @@
 # Date: November 3, 2023
 
 # Greet the user
-print("Hello! I am here to ask how your courses are this semester!")
-
-questions = [
-    "How is your A Block on a scale of 1 to 5?",
-    "How is your B Block on a scale of 1 to 5?",
-    "How is your C Block on a scale of 1 to 5?",
-    "How is your D Block on a scale of 1 to 5?"
-]
+number_of_courses = int(input("Hello! How many courses are you taking?"))
 
 final_score = 0
 
 # Ask them to rate their course from 1 to 5
-for question in questions:
-    print(question)
 
-    rating = int(input().lower().strip(",.?!"))
+for i in range(number_of_courses):
+    rating = int(input(f"how would you rate course {i+1} out of 5? "))
     final_score += rating
-
-# Do some math
-average_score = final_score / len(questions)
+        
+average_score = final_score / number_of_courses
 
 # Present the user their results and add a response
-if average_score in "0, 1":
-    print(f"{average_score:.2f}? Ouch.")
-elif average_score in " ":
-    print(f"{average_score}? Not a bad semester.")
+if average_score <= 1:
+    print(f"{average_score:.1f}? Ouch.")
+elif average_score < 3:
+    print(f"{average_score:.1f}? Not a bad semester.")
+elif average_score >= 3:
+    print(f"{average_score:.1f}? Glad to hear that!")
