72273f75d6c7105c873e0c8cbdc17b346ea134f2
Katrina Chan <khcbunnies@gmail.com>
2023-11-24 14:01:22 -0800

    worked with turtle

---
+++ Python/cookies.py
@@ -0,0 +1,46 @@
+# Cookies
+# Author: Katrina Chan
+# Date: November 22, 2023
+
+import random
+import turtle
+
+# Make baker turtle
+baker_turtle = turtle.Turtle()
+baker_turtle.color("brown")
+
+# Design a function that creates a cookie at x and y
+def make_cookie(x: int, y: int):
+    """Creates a cookie on the screen
+    Draws it at (x, y)
+    
+    Params:
+    x - the x-location of the centre
+    y - the y-location of the centre"""
+    baker_turtle.color("brown")
+    baker_turtle.penup()
+    baker_turtle.goto(-5 + x, -30 + y)
+    baker_turtle.pendown()
+    baker_turtle.circle(30)
+    baker_turtle.penup()
+    baker_turtle.color("black")
+    baker_turtle.goto(0 + x, 0 + y)
+    baker_turtle.stamp()
+# Make chips
+    baker_turtle.goto(10 + x, 10 + y)
+    baker_turtle.stamp()
+    baker_turtle.goto(10 + x, -10 + y)
+    baker_turtle.stamp()
+    baker_turtle.goto(-10 + x, -10 + y)
+    baker_turtle.stamp()
+    baker_turtle.goto(-10 + x, 10 + y)
+    baker_turtle.stamp()
+
+# Create cookie
+for _ in range(10000):
+    offset_x = random.randint(-500, 501)
+    offset_y = random.randint(-500, 501)
+
+    make_cookie(offset_x, offset_y)
+
+turtle.done()
