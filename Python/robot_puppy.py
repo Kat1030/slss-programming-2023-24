2b73df89337e7a194e531a4f197ef4ae1facb110
Katrina Chan <khcbunnies@gmail.com>
2024-01-15 14:19:07 -0800

    worked on robot puppy

--- Python/robot_puppy.py
+++ Python/robot_puppy.py
@@ -11,6 +11,12 @@ import colour_helper
 
 ball_image = Image.open("./Images/Red Ball.jpeg")
 red_pixels = []
+red_y_pixels = []
+red_x_pixels = []
+amount_of_pixels = 0
+pixel_on_y = 0
+pixel_on_x = 0
+
 
 # Visit every pixel in the image
 for y in range(ball_image.height):
@@ -20,5 +26,8 @@ for y in range(ball_image.height):
         # If that pixel is red, store it's location
         if colour_helper.pixel_to_string(current_pixel) == "ball red":
             red_pixels.append((x, y))
+            amount_of_pixels += 1
+
+print(pixel_on_y)
 
 ball_image.close()
